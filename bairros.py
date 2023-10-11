47d9bbe1aa6e6e662f6f7e96d9225f2af5a3bb1c
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-11 14:34:24 -0300

    Update bairros.py

--- bairros.py
+++ bairros.py
@@ -1,3 +1,10 @@
+'''
+PROGRAMA:
+    COLOCA EM ORDEM ALFABETICA
+    REMOVE ACENTOS E CEDILHAS
+    REMOVE DUPLICIDADES EM NOMES
+    REMOVE ESPAÇOS ANTES E DEPOIS DOS NOMES
+'''
 import unicodedata
 
 def remover_acentos(texto):
@@ -6,12 +13,14 @@ def remover_acentos(texto):
 with open('b1.txt', 'r', encoding='utf-8') as arquivo:
     bairros = arquivo.readlines()
 
-bairros_limpos = list(set(remover_acentos(bairro.strip().upper()) for bairro in bairros))
+bairros_limpos = [remover_acentos(bairro.strip().upper()) for bairro in bairros]
 
-bairros_ordenados = sorted(bairros_limpos)
+# Remover bairros duplicados e ordenar
+bairros_unicos_ordenados = sorted(set(bairros_limpos))
 
 with open('bairros.txt', 'w', encoding='utf-8') as arquivo_saida:
-    for bairro in bairros_ordenados:
+    for bairro in bairros_unicos_ordenados:
         arquivo_saida.write(f"{bairro}\n")
 
+print(f"Foram encontrados {len(bairros_limpos) - len(bairros_unicos_ordenados)} bairros duplicados.")
 print("Lista de bairros salva em 'bairros.txt'.")
